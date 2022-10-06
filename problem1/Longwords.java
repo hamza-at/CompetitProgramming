933b823503f5d57e28482ab22fc56118dab600a6
hamza-at <aitmansour.hamza11@gmail.com>
2022-10-07 00:16:28 +0200

    71A - Way Too Long Words (decreasing memory usage)

--- problem1/Longwords.java
+++ problem1/Longwords.java
@@ -5,18 +5,16 @@ public static void main(String args[]) {
 	Scanner in= new Scanner(System.in);
     int n = in.nextInt();
    String a;
-    String[] tab= new String[n];
     for(int i=0;i<n;i++) {
     	a=in.next();
     	if(a.length()>10) {
-    		char begin = a.charAt(0);
-    	      char end = a.charAt(a.length()-1);
-    	      tab[i]=begin+Integer.toString(a.length()-2)+end;
+    	
+    	      System.out.println(a.charAt(0)+Integer.toString(a.length()-2)+a.charAt(a.length()-1));
     	     
     	}else {
-    	tab[i]=a;
+    		 System.out.println(a);
         }
-    	 System.out.println(tab[i]);
+    	
     }
    
     in.close();
